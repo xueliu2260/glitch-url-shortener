47c13d9a323fd234f9f2ad18e846b4766474daeb
System <none>
2017-07-06 01:51:24 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -32,6 +32,30 @@ var dreams = [
   "Climb a really tall mountain",
   "Wash the dishes"
 ];
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = 'mongodb://localhost:27017/glitch-project';      
+//(Focus on This Variable)
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
