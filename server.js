5ba1604eff085fbd9800f8c69359616b0fbbf28b
System <none>
2017-07-06 05:09:52 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -35,37 +35,37 @@ var dreams = [
 var mongodb = require('mongodb');
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
-var test = require('assert');
-// Connection URL. This is where your mongodb server is running.
+// var MongoClient = mongodb.MongoClient;
+// var test = require('assert');
+// // Connection URL. This is where your mongodb server is running.
 
-//(Focus on This Variable)
-var url = 'mongodb://liuerbaozi2260:zja900530@ds137220.mlab.com:37220/glitch-project';      
-//(Focus on This Variable)
+// //(Focus on This Variable)
+// var url = 'mongodb://liuerbaozi2260:zja900530@ds137220.mlab.com:37220/glitch-project';      
+// //(Focus on This Variable)
 
-// Use connect method to connect to the Server
-  MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to ', url);
+// // Use connect method to connect to the Server
+//   MongoClient.connect(url, function (err, db) {
+//   if (err) {
+//     console.log('Unable to connect to the mongoDB server. Error:', err);
+//   } else {
+//     console.log('Connection established to ', url);
 
-    // do some work here with the database.
-    var col = db.collection('createIndexExample1');
-  // Insert a bunch of documents
-    col.insert([{a:1, b:1}
-    , {a:2, b:2}, {a:3, b:3}
-    , {a:4, b:4}], {w:1}, function(err, result) {
-    test.equal(null, err);
-    // Show that duplicate records got dropped
-    col.aggregation({}, {cursor: {}}).toArray(function(err, items) {
-      test.equal(null, err);
-      test.equal(4, items.length);
-      db.close();
-    });
-      });
-  }
-});
+//     // do some work here with the database.
+//     var col = db.collection('createIndexExample1');
+//   // Insert a bunch of documents
+//     col.insert([{a:1, b:1}
+//     , {a:2, b:2}, {a:3, b:3}
+//     , {a:4, b:4}], {w:1}, function(err, result) {
+//     test.equal(null, err);
+//     // Show that duplicate records got dropped
+//     col.aggregation({}, {cursor: {}}).toArray(function(err, items) {
+//       test.equal(null, err);
+//       test.equal(4, items.length);
+//       db.close();
+//     });
+//       });
+//   }
+//});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
