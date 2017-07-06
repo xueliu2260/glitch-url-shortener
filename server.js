b292dfed40a5a172d14211003f9353b5fb9ce1e6
System <none>
2017-07-06 05:20:52 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -35,20 +35,20 @@ var dreams = [
 var mongodb = require('mongodb');
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-// var MongoClient = mongodb.MongoClient;
-// var test = require('assert');
+var MongoClient = mongodb.MongoClient;
+var test = require('assert');
 // // Connection URL. This is where your mongodb server is running.
 
 // //(Focus on This Variable)
-// var url = 'mongodb://liuerbaozi2260:zja900530@ds137220.mlab.com:37220/glitch-project';      
+var url = 'mongodb://liuerbaozi2260:zja900530@ds137220.mlab.com:37220/glitch-project';      
 // //(Focus on This Variable)
 
 // // Use connect method to connect to the Server
-//   MongoClient.connect(url, function (err, db) {
-//   if (err) {
-//     console.log('Unable to connect to the mongoDB server. Error:', err);
-//   } else {
-//     console.log('Connection established to ', url);
+MongoClient.connect(url, function (err, db) {
+if (err) {
+  console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+  console.log('Connection established to ', url);
 
 //     // do some work here with the database.
 //     var col = db.collection('createIndexExample1');
@@ -66,7 +66,42 @@ var mongodb = require('mongodb');
 //       });
 //   }
 //});
+var docs = [{
+      title : "this is my title", author : "bob", posted : new Date() ,
+      pageViews : 5, tags : [ "fun" , "good" , "fun" ], other : { foo : 5 },
+      comments : [
+        { author :"joe", text : "this is cool" }, { author :"sam", text : "this is bad" }
+      ]}];
 
+  // Create a collection
+  var collection = db.collection('aggregation_each_example');
+  // Insert the docs
+  collection.insertMany(docs, {w: 1}, function(err, result) {
+
+    // Execute aggregate, notice the pipeline is expressed as an Array
+    var cursor = collection.aggregate([
+        { $project : {
+          author : 1,
+          tags : 1
+        }},
+        { $unwind : "$tags" },
+        { $group : {
+          _id : {tags : "$tags"},
+          authors : { $addToSet : "$author" }
+        }}
+      ], { cursor: { batchSize: 1 } });
+
+    // Get all the aggregation results
+    cursor.each(function(err, docs) {
+      test.equal(null, err);
+
+      if(docs == null) {
+        db.close();
+      }
+    });
+  });
+}
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
